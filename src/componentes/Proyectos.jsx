6b269528c8a65d37c7b39876b28bbff0501658ab
mabel <wendy.torrico.wt@gmail.com>
2022-08-11 17:04:47 -0500

    arreglos proyectos

--- src/componentes/Proyectos.jsx
+++ src/componentes/Proyectos.jsx
@@ -6,6 +6,8 @@ import easy from "../imagenes/easy.png";
 import ey from "../imagenes/ey.png";
 import cesar from "../imagenes/chiper.png"
 import portafolio from "../imagenes/portafolio.png"
+import camisas from "../imagenes/camisas.png"
+import burger from "../imagenes/burger.png"
 
 
 
@@ -14,7 +16,7 @@ export default function Proyectos() {
   return (
     <>
       <Footer />
-      <section>
+      <section className="conteiner-proyectos">
         <h2>Mis proyectos</h2>
 
         <div className="conteiner-pro">
@@ -25,13 +27,11 @@ export default function Proyectos() {
               <img src={cesar} alt="" />
             </div>
             <h3>Cifrado cesar</h3>
-            <p>Interfaz web inspirada en la serie animada Rick and Morty,
-              a través de filtros el usuario puede acceder a información
-              de cada personaje.
+            <p> Pagina Web creada para incriptar contraseñas usando el codigo cesar.
               El proyecto se desarrollo con HTML, CSS y Vainilla JS.</p>
             <div className="proyecto-btn-conteiner">
               <a class="home-button proyecto-btn" href="https://golden-chebakia-94b8d4.netlify.app/" target="blank">ver pagina</a>
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/chiper" target="blank">Repositorio</a>
             </div>
           </div>
 
@@ -47,7 +47,7 @@ export default function Proyectos() {
               El proyecto se desarrollo con HTML, CSS y Vainilla JS.</p>
             <div className="proyecto-btn-conteiner">
               <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">ver pagina</a>
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/SCL018-data-lovers" target="blank">Repositorio</a>
             </div>
           </div>
 
@@ -60,18 +60,21 @@ export default function Proyectos() {
             <p>Red social enfocada a las personas vegetarianas. Para el
               desarrollo del proyecto se utilizo HTML, CSS, Vainilla JS y Firebase</p>
             <div className="proyecto-btn-conteiner">
-              <a class="home-button proyecto-btn" href="https://easy-veggie-scl018.web.app">ver pagina</a>
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://easy-veggie-scl018.web.app "target="blank">ver pagina</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/SCL018-social-network" target="blank">Repositorio</a>
             </div>
           </div>
 
           {/* proyecto Burger Queen< */}
           <div className="proyecto-conteiner">
+          <div className="img-conteiner">
+              <img src={burger} alt="" />
+            </div>
             <h3>Burger Queen</h3> {/* falta enlace */}
             <p>Aplicacion web para generar pedidos a un restaurante de manera online </p>
             <div className="proyecto-btn-conteiner">
-              <a class="home-button proyecto-btn" href="https://easy-veggie-scl018.web.app">ver pagina</a>
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://incredible-elf-ba2f1d.netlify.app/" target="blank">ver pagina</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/SCL018-burger-queen" target="blank">Repositorio</a>
             </div>
           </div>
 
@@ -82,28 +85,39 @@ export default function Proyectos() {
             </div>
             <h3>EY INSURANCE: APLICACIÓN PARA COBRANZA DE SEGURO AUTOMOTRIZ</h3>
             <p>Proyecto Ganador del primer lugar en el Talent Fest Chile 2022 de Laboratoria</p>
+            <p>Para ingresar:
+
+                  correo: atolosab@gmail.com //
+                  contraseña: ejemplo01
+            </p>
             <div className="proyecto-btn-conteiner">
-              <a class="home-button proyecto-btn" href="https://ey-insurance-5a2d0.web.app/">ver pagina</a>
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://ey-insurance-5a2d0.web.app/" target="blank">ver pagina</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/EY-Insurance" target="blank">Repositorio</a>
             </div>
           </div>
 
           {/* proyecto Tienda en linea */}
           <div className="proyecto-conteiner">
+          <div className="img-conteiner">
+              <img src={camisas} alt="" />
+            </div>
             <h3>Tienda en linea</h3> {/* falta enlace */}
             <p>Interfaz web que simula una tienda online de camisetas.Proyecto generado con HTML y CSS </p>
             <div className="proyecto-btn-conteiner">
-              <a class="home-button proyecto-btn" href="https://ey-insurance-5a2d0.web.app/">ver pagina</a>
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://cosmic-sawine-47c405.netlify.app/" target="blank">ver pagina</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/tienda-camisetas" target="blank">Repositorio</a>
             </div>
           </div>
 
           {/* proyecto Blog cafe */}
           <div className="proyecto-conteiner">
+          <div className="img-conteiner">
+              <img src={burger} alt="" />
+            </div>
             <h3>Blog cafe</h3> {/* falta enlace */}
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque ex quae, tempore ipsum dicta reprehenderit repellendus eos, facere debitis possimus rerum, explicabo voluptatum rem. Fugiat inventore illo porro ratione ullam!</p>
             <div className="proyecto-btn-conteiner">
-              <a class="home-button proyecto-btn" href="https://ey-insurance-5a2d0.web.app/">ver pagina</a>
+              <a class="home-button proyecto-btn" href="https://ey-insurance-5a2d0.web.app/" target="blank">ver pagina</a>
               <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
             </div>
           </div>
@@ -114,9 +128,11 @@ export default function Proyectos() {
               <img src={portafolio} alt="" />
             </div>
             <h3>portafolio</h3> {/* falta enlace */}
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae officia placeat deleniti aperiam culpa eius. Fuga quaerat aliquid aut fugit possimus in doloribus consectetur fugiat odit commodi! Mollitia, veniam fugit.</p>
+            <p>Pagia web que aloja mi informacion y datos.
+            HTML, CSS, Vainilla JS y React.         
+            </p>
             <div className="proyecto-btn-conteiner">
-              <a class="home-button proyecto-btn" href="https://kyoyarzun.github.io/SCL018-data-lovers/src/" target="blank">Repositorio</a>
+              <a class="home-button proyecto-btn" href="https://github.com/mabelRT/portafolio-react" target="blank">Repositorio</a>
             </div>
           </div>
         </div>
